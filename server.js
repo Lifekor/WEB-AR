a7e22190ed209589ec285f8bef3b88f5cdd904fd
Lifekor <45297055+Lifekor@users.noreply.github.com>
2019-09-25 19:00:33 +0300

    fix server index

--- server.js
+++ server.js
@@ -3,15 +3,8 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 8080;
 
-app.set("/", "html");
-app.use(express.static(path.join(__dirname, "/")));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+app.use(express.static("public"));
 
 
 app.listen(port);
