c52efe90efaab6fa4d07cadcf7977ea6538d6d18
Lifekor <45297055+Lifekor@users.noreply.github.com>
2019-09-25 17:55:03 +0300

    fix server render

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 8080;
 
+app.set("/", "html");
+app.use(express.static(path.join(__dirname, "/")));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 
 app.listen(port);
 console.log("App listening on port " + port);
